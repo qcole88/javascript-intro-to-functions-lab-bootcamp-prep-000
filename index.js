5c032963ec1bf8d80c1bd67ca2ed0bd1f9a729f3
solarfive.mgmt@gmail.com <solarfive.mgmt@gmail.com>
2018-07-16 20:04:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function shout(string) {
   return                                                     string.toUpperCase()
   }
 function whisper(string) {return string.toLowerCase() }
-function logShout(string) {console.log; string.toLowerCase() }
+function logShout(string) {console.log; 'HELLO'.toLowerCase() }
+
 
   
